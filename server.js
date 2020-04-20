72ba83bfec04901742c565fc06ad04086e3f8336
Thad Tayo <josh_tayo@yahoo.com>
2020-04-20 19:32:35 -0400

    Added body-parser... Express built-in bp is not working.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // Define modules
 const express = require('express');
+const Bodyparser = require('body-parser');
+
 const customers = require('./routes/api/customers');
 const flights = require('./routes/api/flights');
 
 const app = express();
 
 // Add Bodyparser
-app.use(express.json());
+app.use(Bodyparser.json()); // idk why tf express.json() built-in bodyparser won't work. Settling for this
+app.use(Bodyparser.urlencoded({ extended: true}));
 
 // Routes configuration
 app.use('/api/customers', customers);
