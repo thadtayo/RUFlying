473e3ccab10dc51291a4085d0a43730cf2a73cb4
bplotnick0 <bplotnick0@gmail.com>
2020-04-22 14:56:01 -0400

    login page

---
+++ ru-flying/src/components/login/register.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import loginImg from '../../yeKcim-plane.svg'
+
+export class Register extends React.Component {
+    constructor(props) {
+    super(props)
+    }
+
+    render() {
+        return (
+            <div className = "base-container" ref= {this.props.containerRef}>
+                <div className = "header">Register</div>
+                <div className = "content">
+
+                   <div className = "image">
+                     <img src = {loginImg} />  
+                    </div>
+
+                    <div className = "form">
+                        <div className = "form-group">
+                            <label htmlFor = "username">First Name</label>
+                            <input type="firstname" name =" firstname" placeholder = "First Name"/>
+                        </div>
+                        <div className = "form-group">
+                            <label htmlFor = "username">Last Name</label>
+                            <input type="lastname" name =" lastname" placeholder = "Last Name"/>
+                        </div>
+                        <div className = "form-group">
+                            <label htmlFor = "email">Email</label>
+                            <input type="text" name =" email" placeholder = "email"/>
+                        </div>
+                        <div className = "form-group">
+                            <label htmlFor = "password">Password</label>
+                            <input type="password" name ="password" placeholder = "password"/>
+                        </div>
+                    </div> 
+                </div>
+                <div className = "footer">
+                    <button type = "button" className = "btn">Register</button>
+                </div>
+
+
+            </div>
+        )
+    }
+}
